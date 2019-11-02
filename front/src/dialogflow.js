469405a9651ffa9d2724f182422aafce214daece
donghee <dongheepark.dev@gmail.com>
2019-11-02 15:32:12 +0900

    front add

---
+++ front/src/dialogflow.js
@@ -0,0 +1,42 @@
+
+//dialogflow와 서버 연결
+const dialogflow = require('dialogflow');
+const uuid = require('uuid');
+
+/**
+ * Send a query to the dialogflow agent, and return the query result.
+ * @param {string} projectId The project to be used
+ */
+async function runSample(projectId = 'your-project-id') {
+  // A unique identifier for the given session
+  const sessionId = uuid.v4();
+
+  // Create a new session
+  const sessionClient = new dialogflow.SessionsClient();
+  const sessionPath = sessionClient.sessionPath(projectId, sessionId);
+
+  // The text query request.
+  const request = {
+    session: sessionPath,
+    queryInput: {
+      text: {
+        // The query to send to the dialogflow agent
+        text: '반디봇과 서버를 연결해결해줘!',
+        // The language used by the client (en-US)
+        languageCode: 'ko',
+      },
+    },
+  };
+
+  // Send request and log result
+  const responses = await sessionClient.detectIntent(request);
+  console.log('Detected intent');
+  const result = responses[0].queryResult;
+  console.log(`  Query: ${result.queryText}`);
+  console.log(`  Response: ${result.fulfillmentText}`);
+  if (result.intent) {
+    console.log(`  Intent: ${result.intent.displayName}`);
+  } else {
+    console.log(` No intent matched.`);
+  }
+}
